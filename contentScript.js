7fbff35ed5ee61a32d87c3c7a7c30acb75f85521
Gabor Csapo <gabor.csapo@nyu.edu>
2020-04-20 21:51:56 +0800

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -2,7 +2,6 @@
 let focusIdxOnPage = 0;
 let searchResultArray = Array.prototype.slice.call(document.getElementById("rso").children).filter(block => block.className == "g").map(e => e.getElementsByClassName("r")[0].children[0]);
 
-//window.scrollTo(0,0);
 searchResultArray[0].focus();                                                          
                                                         
 document.addEventListener('keydown', (e)=>{
