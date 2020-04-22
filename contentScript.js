2ae58323d0d8e3706d53c0fdd7854056fd5f1eed
Gabor <gabor.csapo@nyu.edu>
2020-04-22 22:56:47 +0800

    commit new version

--- contentScript.js
+++ contentScript.js
@@ -1,21 +1,21 @@
-
 let focusIdxOnPage = 0;
 let searchResultArray = Array.prototype.slice.call(document.getElementById("rso").children).filter(block => block.className == "g").map(e => e.getElementsByClassName("r")[0].children[0]);
 
 searchResultArray[0].focus();                                                          
                                                         
 document.addEventListener('keydown', (e)=>{
-  if (e.code == 'ArrowUp'){
+  if (e.code == 'Tab'){
     e.preventDefault();
-    if (focusIdxOnPage > 0){
-      focusIdxOnPage -= 1;
-      searchResultArray[focusIdxOnPage].focus();
-    }
-  } else if (e.code == 'ArrowDown'){
-    e.preventDefault();
-    if (focusIdxOnPage < searchResultArray.length - 1){
-      focusIdxOnPage += 1;
-      searchResultArray[focusIdxOnPage].focus();
+    if (event.shiftKey){
+      if (focusIdxOnPage > 0){
+        focusIdxOnPage -= 1;
+        searchResultArray[focusIdxOnPage].focus();
+      }
+    } else {
+      if (focusIdxOnPage < searchResultArray.length - 1){
+        focusIdxOnPage += 1;
+        searchResultArray[focusIdxOnPage].focus();
+      }
     }
   }
   console.log(searchResultArray[focusIdxOnPage].href);
